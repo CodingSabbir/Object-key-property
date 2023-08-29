add54ada482c75ef046fc2f87aa2ca158d186be4
sabbir <mdsabbirrahmanm740@gmail.com>
2023-08-29 19:55:49 +0600

    first commit

---
+++ call and apply method/main.js
@@ -0,0 +1,37 @@
+const user={
+    name:'sabbir',
+    address:'bogura',
+    phone:122455,
+    salary:7000,
+    getFullName: function(){
+       return `${this.name} ${this.address}`
+    },
+    chargeBill:function(amount,tax){
+      this.salary=this.salary-amount-tax
+    return  this.salary
+    }
+};
+
+
+const heroUser={
+    name:'rakib',
+    id:2010,
+    salary:5000,
+};
+
+
+const localUser={
+    name:'dev',
+    id:664,
+    salary:10000,
+};
+
+
+user.chargeBill.call(heroUser,4000,20);
+console.log(heroUser.salary);
+
+
+user.chargeBill.apply(localUser,[1000,20]);
+console.log(localUser.salary);
+
+
